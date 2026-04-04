4ed4fe22c63689f4f165b631219fdba74060f2c3
ChardonneDixon <chardonne.dixon@gmail.com>
2026-04-04 13:32:14 -0500

    updated firePottery module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,29 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
+import { firePottery } from "./Kiln.js"
+
+
+const mug = makePottery("mug", 11, 4)
+const bowl = makePottery("bowl", 17, 7)
+const vase = makePottery("vase", 29, 11)
+const plate = makePottery("plate", 4, 1)
+const cup = makePottery("cup", 3, 2)
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-//console.log(makePottery("mug", 10, 3))
-
-
+const firedMug = firePottery(mug, 2000)
+const firedBowl = firePottery(bowl, 2100)
+const firedVase = firePottery(vase, 2300)   // this one cracks
+const firedPlate = firePottery(plate, 1800)
+const firedCup = firePottery(cup, 2400)     // this one cracks
 // Fire each piece of pottery in the kiln
+
+console.log(firedMug)
+console.log(firedBowl)
+console.log(firedVase)
+console.log(firedPlate)
+console.log(firedCup)
+
 
 
 // Determine which ones should be sold, and their price
